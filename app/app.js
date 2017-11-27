8bfd052075faad7db6e606052d3525322acc6dde
linweiwei <linweiwei@xiaoyouzi.com>
2017-11-27 11:35:40 +0800

    1212

--- app/app.js
+++ app/app.js
@@ -22,11 +22,11 @@ var accessLogStream = rfs('access.log', {
 });
 
 process.on('uncaughtException',function(err){
-    error("今天天气不错了！！！：" + (err.stack)||err);
+    error("我是郭书兴！！！：" + (err.stack)||err);
 });
 
-var a = 1000;
-var b = a+1;
+var a = '郭书兴提交';
+var b = a+10;
 /**
  * 初始化 MySQL Connection
  */
